be8ac0f326edfe25eb945a8a183a5e93449cdbbe
KUNAL MEENA <kunalmeena1311@gmail.com>
2025-07-17 16:19:01 +0530

    first commit

---
+++ src/main/java/Kunal88591/kuJournal/KuJournalApplication.java
@@ -0,0 +1,13 @@
+package Kunal88591.kuJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kunal88591/kuJournal/KuJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package Kunal88591.kuJournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
